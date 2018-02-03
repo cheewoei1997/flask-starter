5e25c3cefef470d6b8b1acdb2f11a101636e2a2b
cheewoei1997 <cheewoei1997@gmail.com>
2018-02-03 20:59:32 +0800

    heroku try

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('postgresql://postgres:@localhost:5432/flask-demo', convert_unicode=True)
+engine = create_engine("""postgres://stucwlpmyfovvx:310b7dd6b0793868468866558043b0ffaadbc5685ba76bd45846dacd2b64367a@ec2-50-16-231-2.compute-1.amazonaws.com:5432/dbf8uo3mahb4ok
+""", convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
